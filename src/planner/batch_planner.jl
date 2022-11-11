92809f4143bd3f7d27566f985a412136a060d0d7
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2022-11-11 22:55:49 +0200

    Include batch planner into package.

---
+++ src/planner/batch_planner.jl
@@ -0,0 +1,114 @@
+using ProgressMeter
+using DataFrames: DataFrame, empty!, push!, nrow
+using CSV
+using Dates
+using Random: shuffle!
+
+function planner_batch(;
+    budget=round.(Int, exp10.(range(3, 6, 31))),
+    γ=0.96:0.005:0.995,
+    horizon=20:5:60,
+    exploration_param=[8],
+    seed_shift=1:10,
+    file_name::Union{Nothing,String}=nothing,
+    starting_seed=4839633
+)
+    seeds = starting_seed .+ seed_shift
+
+    it = collect(Base.product(horizon, budget, exploration_param, γ, seeds))
+    shuffle!(it)
+    if isnothing(file_name)
+        return it
+    end
+
+    dff = CSV.read(file_name * ".csv", DataFrame)
+    isinit(it) = isempty(
+        filter(
+            [:horizon, :budget, :exploration_param, :gamma, :seed] =>
+                (h, b, t, g, s) ->
+                    (h, b, t, g, s) == (it[1], it[2], it[3], it[4], it[5]),
+            dff,
+        ),
+    )
+    return filter(isinit, it)
+end
+
+# function sp_batch(file_name::Union{Nothing,String}=nothing,)
+#     planner_batch(
+#         budget=round.(Int, exp10.(range(2, 5, 31))),
+#         horizon=10:5:40,
+#         exploration_param=[4, 8, 16, 32, 64],
+#         file_name=file_name
+#     )
+# end
+
+function execute_batch(
+    planner_it,
+    file_name_prefix,
+    opts=PendulumOpts();
+    envfun=InvertedDoublePendulumEnv,
+    show_progress=false,
+    start_new_file=true
+)
+    file_name = file_name_prefix * ".csv"
+    if start_new_file && isfile(file_name)
+        @error "File already exists! Aborting."
+        return nothing
+    elseif !start_new_file && !isfile(file_name)
+        @error "File did not exists, while it is expected! Aborting."
+        return nothing
+    end
+    nit = length(planner_it)
+    date_format = "yyyy-mm-dd HH:MM:SS"
+    @info string(
+        Dates.format(now(), date_format),
+        " Expected to get ",
+        length(planner_it),
+        " results!",
+    )
+
+    count = 0
+    lk = Threads.ReentrantLock()
+    df = DataFrame(
+        seed=12554,
+        horizon=40,
+        budget=1_000_000,
+        exploration_param=500,
+        gamma=0.975,
+        steps=100,
+    )
+    empty!(df)
+    p = !show_progress || Progress(length(planner_it))
+    Threads.@threads for j in eachindex(planner_it)
+        env = envfun(opts)
+        mcts = Planner(
+            env,
+            seed=planner_it[j][5],
+            horizon=planner_it[j][1],
+            budget=planner_it[j][2],
+            exploration_param=planner_it[j][3],
+            γ=planner_it[j][4],
+        )
+        res = run_planner!(mcts)
+        !show_progress || next!(p)
+        lock(lk) do
+            push!(df, [planner_it[j][5], planner_it[j][1], planner_it[j][2], planner_it[j][3], planner_it[j][4], res])
+            if nrow(df) >= 10
+                count += nrow(df)
+                CSV.write(file_name, df, append=!start_new_file)
+                start_new_file = false
+                empty!(df)
+                show_progress ||
+                    @info "$(Dates.format(now(), date_format)) Iteration $count/$nit"
+            end
+        end
+    end
+
+    if nrow(df) > 0
+        count += nrow(df)
+        CSV.write(file_name, df, append=!start_new_file)
+        empty!(df)
+        show_progress || @info "$(Dates.format(now(), date_format)) Iteration $count/$nit"
+    end
+    nothing
+end
